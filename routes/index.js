23d6470ec23d2927c600a0297114a2252d594fd7
nelfimov <nelfimov@mail.ru>
2022-11-28 21:41:52 +0300

    added footer, base template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 import {Router} from 'express';
 const router = Router();
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 router.get('/', (req, res, next) => {
-  res.render('pages/index');
+  res.render('pages/index', {messages});
 });
 
 export default router;
